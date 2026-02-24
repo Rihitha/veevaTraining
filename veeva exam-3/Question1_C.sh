b37c5f6afb781ca86cc8973fea8b79af151072e2
NERELLA N S VENKATA SIVA SAI RIHITHA <harisubrahmanyam696@gmail.com>
2026-02-24 14:44:49 +0530

    Delete veeva exam-3 directory

--- veeva exam-3/Question1_C.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Search for files larger than 1KB in the current directory
-echo "Files larger than 1KB:"
-find . -maxdepth 1 -type f -size +1k
--- veeva exam-3/Question3_B.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Intentionally generate an error and send it to error.log
-ls nonexistentfile 2>> error.log
-
-echo "Error logged to error.log"
